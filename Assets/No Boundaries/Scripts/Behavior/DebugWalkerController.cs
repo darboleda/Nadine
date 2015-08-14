63aa1775c85dd7ce42407e64af38509b39842e42
Mors <morsangelos@hotmail.com>
2015-08-13 21:12:24 -0700

    Starting on refactoring Nadine's movement code.

---
+++ Assets/No Boundaries/Scripts/Behavior/DebugWalkerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugWalkerController : MonoBehaviour {
+
+	public WalkerPhysics Physics;
+
+	public void Update()
+	{
+		float hAxis = Input.GetAxisRaw("Horizontal");
+		float vAxis = Input.GetAxisRaw("Vertical");
+		
+		Vector3 movement = new Vector2(hAxis, vAxis);
+		float speed = (movement.sqrMagnitude < 0.001f ? 0 :
+		         	   movement.sqrMagnitude > 1 ? 1 :
+		         	   movement.magnitude);
+
+		float direction = Physics.Direction;
+		if (speed > 0)
+		{
+			direction = Mathf.Atan2(vAxis, hAxis) * Mathf.Rad2Deg;
+		}
+
+		Physics.Speed = speed;
+		Physics.Direction = direction;
+	}
+}
---
+++ Assets/No Boundaries/Scripts/Behavior/WalkerPhysics.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkerPhysics : MonoBehaviour {
+
+	[System.Serializable]
+	public struct WorldCoordinates
+	{
+		public Vector3 North;
+		public Vector3 East;
+		
+		public Vector3 Transform(float direction, float speed)
+		{
+			float radDirection = direction * Mathf.Deg2Rad;
+			return speed * (North * Mathf.Sin(radDirection) + East * Mathf.Cos(radDirection));
+		}
+	}
+
+
+	[System.Serializable]
+	public struct CollisionConfig
+	{
+		public Vector3 PositionOffset;
+		public float Radius;
+		public LayerMask Mask;
+	}
+
+	public CollisionConfig Collision;
+	public WorldCoordinates World;
+    
+    public float Direction;
+    public float Speed;
+
+	public float AccelerationMultiplier = 50;
+	public float MaxSpeedMultiplier = 5;
+	public float FrictionMultiplier = 50;
+
+	private Vector3 velocity;
+    
+    public void FixedUpdate()
+    {
+        Vector3 velocityDirection = velocity.normalized;
+		float calculatedSpeed = Mathf.Clamp(Speed, -1, 1);
+        
+		float targetSpeed = calculatedSpeed * MaxSpeedMultiplier;
+		Vector3 targetVelocity = World.Transform(Direction, targetSpeed);
+		Vector3 accelerationDirection = (targetVelocity - velocity).normalized;
+
+
+		float accelerationAngle = accelerationDirection.GetAngle(velocity, Vector3.forward);
+
+		Vector3 acceleration;
+		if (targetVelocity.sqrMagnitude < 0.01f)
+		{
+			acceleration = -velocityDirection * FrictionMultiplier;
+		}
+		else if (velocity.sqrMagnitude < 0.01f)
+		{
+			acceleration = accelerationDirection * AccelerationMultiplier;
+		}
+		else if (Mathf.Abs(accelerationAngle) > 90)
+		{
+			acceleration = (accelerationDirection * AccelerationMultiplier) - (velocityDirection * FrictionMultiplier);
+		}
+		else
+		{
+			acceleration = accelerationDirection * AccelerationMultiplier;
+		}
+
+		Vector3 newVelocity = velocity + acceleration * Time.deltaTime;
+		if (Vector3.Dot(newVelocity - targetVelocity, velocity - targetVelocity) < 0)
+		{
+			newVelocity = targetVelocity;
+		}
+		velocity = newVelocity;
+
+        
+        Vector3 initialDelta = velocity * Time.deltaTime;
+        Vector3 correctedDelta = CheckCollision2D(initialDelta);
+
+        transform.Translate(correctedDelta);
+    }
+
+	private Vector3 CheckCollision2D(Vector3 delta)
+    {
+        Vector2 delta2D = delta;
+        Vector2 position2D = transform.position + Collision.PositionOffset;
+        Vector2 direction = delta2D.normalized;
+        float distance = delta2D.magnitude;
+
+        Debug.DrawLine(position2D, position2D + direction * distance * 10, Color.red);
+
+
+        RaycastHit2D hit = Physics2D.CircleCast(position2D, Collision.Radius, direction, distance, Collision.Mask.value);
+        if (hit.collider == null) return delta;
+
+        if (Vector2.Dot(direction, hit.normal) >= 0) return delta;
+
+        Vector2 delta2Dremainder = delta2D;
+        delta2D = (hit.point + hit.normal * Collision.Radius) - position2D;
+        delta2Dremainder -= delta2D;
+        
+        delta2Dremainder -= Vector2.Dot(hit.normal, delta2Dremainder) * hit.normal;
+
+        direction = delta2Dremainder.normalized;
+        distance = delta2Dremainder.magnitude;        
+        position2D = position2D + delta2D;
+
+        Debug.DrawLine(position2D, position2D + direction * distance * 10, Color.cyan);
+
+        hit = Physics2D.CircleCast(position2D, Collision.Radius, direction, distance, Collision.Mask.value);
+        
+        if (hit.collider == null) return new Vector3(delta2D.x + delta2Dremainder.x, delta2D.y + delta2Dremainder.y, delta.z);
+        if (Vector2.Dot(direction, hit.normal) >= 0) return new Vector3(delta2D.x + delta2Dremainder.x, delta2D.y +delta2Dremainder.y, delta.z);
+
+        Debug.DrawLine(hit.point, hit.point + hit.normal, Color.magenta);
+        delta2Dremainder = (hit.point + hit.normal * Collision.Radius) - position2D;
+
+        delta2D = delta2D + delta2Dremainder;
+
+        return new Vector3(delta2D.x, delta2D.y, delta.z);
+    }
+}
